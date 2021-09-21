8dc6ebc7d481e264098b0ebac07bdc0fbdc56a6f
damptmann <damptmann@gmail.com>
2021-09-21 16:32:40 -0500

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
+import { CartComponent } from './cart/cart.component';
 
 
 @NgModule({
